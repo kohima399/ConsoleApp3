61f6e5cff49e95de9afe2f1607ad36cace115f4f
kohima399 <ikingagario@gmail.com>
2025-09-25 21:12:20 +0300

    Выполнен ввод значения x.

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите значение параметра x = ");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
         }
     }
 }
